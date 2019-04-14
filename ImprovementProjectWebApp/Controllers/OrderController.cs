44ffebbf61ec0a5538abc27d0abec192a5e73a7b
yliu84 <yliu84@studentmail.nait.ca>
2019-04-13 19:45:23 -0600

    added AddOrder and UserOrderList management

--- ImprovementProjectWebApp/Controllers/OrderController.cs
+++ ImprovementProjectWebApp/Controllers/OrderController.cs
@@ -5,6 +5,8 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using ImprovementProjectWebApp.Data;
 using ImprovementProjectWebApp.Models;
+using ImprovementProjectWebApp.Models.UserPlanViewModels;
+using ImprovementProjectWebApp.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -73,6 +75,69 @@ namespace ImprovementProjectWebApp.Controllers
 
             return View(userPlans);
 
+        }
+
+        [Authorize]
+        public async Task<IActionResult> AddOrder(string userId)
+        {
+            if(userId == null)
+            {
+                return NotFound();
+            }
+
+            UserPlanViewModel userPlan = new UserPlanViewModel()
+            {
+                AppUserPlan = new Models.AppUserPlan(),
+                ApplicationUser =await _db.CustomerProfile.Include(c => c.ApplicationUser).FirstOrDefaultAsync(c => c.ApplicationUserId == userId),
+                AppUserPlans = await _db.AppUserPlans.Include(a => a.PlanPackage).Where(a => a.ApplicationUserId == userId).ToListAsync(),
+                PlanPackages = await _db.PlanPackage.ToListAsync()
+            };
+
+            return View(userPlan);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> UserList()
+        {
+
+            var userList = await _db.CustomerProfile.Include(c => c.ApplicationUser).ToListAsync();
+
+            return View(userList);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> AddOrder(UserPlanViewModel userPlan)
+        {
+            try
+            {
+                AppUserPlan appUserPlan = new AppUserPlan();
+                appUserPlan = userPlan.AppUserPlan;
+                appUserPlan.ApplicationUserId = userPlan.ApplicationUser.ApplicationUserId;
+                appUserPlan.PaymentDate = DateTime.Now;
+                appUserPlan.PaymentType = "Cash";
+                appUserPlan.PlanPackageId = userPlan.PlanId;
+                appUserPlan.PaymentStatus = SD.PaymentStatusApproved;
+                appUserPlan.Status = SD.StatusCompleted;
+                appUserPlan.UserName = userPlan.ApplicationUser.Name;
+                appUserPlan.Phone = userPlan.ApplicationUser.PhoneNumber;
+
+
+                await _db.AppUserPlans.AddAsync(appUserPlan);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(UserOrderList));
+            }
+            catch(Exception e)
+            {
+                userPlan.AppUserPlans = await _db.AppUserPlans.Include(a => a.PlanPackage).Where(a => a.ApplicationUserId == userPlan.ApplicationUser.ApplicationUserId).ToListAsync();
+                userPlan.PlanPackages = await _db.PlanPackage.ToListAsync();
+
+                return View(userPlan);
+            }
+            
+
+           
         }
 
 
---
+++ ImprovementProjectWebApp/Models/UserPlanViewModels/UserPlanViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImprovementProjectWebApp.Models.UserPlanViewModels
+{
+    public class UserPlanViewModel
+    {
+        public AppUserPlan AppUserPlan { get; set; }
+        public CustomerProfile ApplicationUser { get; set; }
+        public int PlanId { get; set; }
+
+        public IEnumerable<AppUserPlan> AppUserPlans { get; set; }
+        public IList<PlanPackage> PlanPackages { get; set; }
+    }
+}
